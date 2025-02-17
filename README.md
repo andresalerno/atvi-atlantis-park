2bc476fdfae65e6ede317e5a351063586c999d92
andresalerno <andresalerno22@gmail.com>
2025-02-17 08:47:01 -0300

    desenvolvimento da atvi

---
+++ README.md
@@ -0,0 +1,36 @@
+```ts
+
+git remote remove origin
+
+
+```
+
+```ts
+
+git remote add origin https://github.com/andresalerno/atvi-atlantis-park.git
+
+```
+
+```ts
+
+git remote -v
+
+```
+
+```ts
+
+git checkout -b desenvolvimento
+
+```
+
+```ts
+
+git push -u origin desenvolvimento
+
+```
+
+```ts
+
+npm install -g ts-node
+
+```
--- teste/index.ts
+++ teste/index.ts
@@ -1,10 +1,13 @@
 import Cliente from "../modelos/cliente";
 import Endereco from "../modelos/endereco";
+import Telefone from "../modelos/telefone";
+
 let cliente = new Cliente()
 cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
 cliente.nomeSocial = `Dom Pedro II`
 cliente.dataCadastro = new Date(1840, 6, 23)
 cliente.dataNascimento = new Date(1825, 11, 2)
+
 let endereco = new Endereco()
 endereco.rua = `R. do Catete`
 endereco.bairro = `Copacabana`
@@ -14,6 +17,11 @@ endereco.pais = `Brasil`
 endereco.codigoPostal = `22220-000`
 cliente.endereco = endereco
 
+let telefone = new Telefone()
+telefone.ddd = `21`
+telefone.numero = `99999-9999`
+cliente.telefones.push(telefone)
+
 let dependente = new Cliente()
 dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
 dependente.nomeSocial = `Princesa Isabel`
@@ -21,6 +29,7 @@ dependente.dataCadastro = new Date(1921, 10, 14)
 dependente.dataNascimento = new Date(1846, 6, 29)
 dependente.endereco = (cliente.endereco.clonar() as Endereco)
 dependente.titular = cliente
+dependente.telefones.push(telefone.clonar() as Telefone)
 cliente.dependentes.push(dependente)
 
 console.log(cliente);
